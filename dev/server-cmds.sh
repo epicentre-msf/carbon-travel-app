3ce92b8fb327cd03cba4bcb839e11f5e5dcac910
Paul Campbell <pacampbell91@gmail.com>
2024-06-07 17:24:19 +0200

    use full_amex_wagram_cwt.rds

--- dev/server-cmds.sh
+++ dev/server-cmds.sh
@@ -1,6 +1,6 @@
-rsync -zavh ~/epicentre/meeting_place_planner/data episerv:/home/epicentre/carbon-travel-app/
+rsync -zavh ~/epicentre/carbon-travel-app/data episerv:/home/epicentre/carbon-travel-app/
 
 docker run --rm \
 -p 5858:3838 \
--v /home/epicentre/carbon-travel-app:/root/app epicentremsf/epishiny \
+-v /home/epicentre/carbon-travel-app:/root/app epicentremsf/epishiny:ubuntu \
 R -e "shiny::runApp('/root/app', port = 3838, host = '0.0.0.0')"
